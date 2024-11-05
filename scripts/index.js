3e734171f04ebcacf893e5cc3739f1971816bdc6
Maksym <max.mixam07.max@gmail.com>
2024-11-05 21:37:27 +0200

    зробив все кроми пароля

--- scripts/index.js
+++ scripts/index.js
@@ -61,50 +61,22 @@ class App {
         console.log(this.notes)
     }
 
-    generateHTML(item, i) {
-        this.wrapper.innerHTML += `
-            <div class="bg-[#3A3644] p-6 rounded-xl w-full relative">
-                <div class="text-3xl font-light pb-6 relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full
-                    before:h-[1px] before:bg-[#77747D] overflow-hidden text-ellipsis whitespace-nowrap">${item.value}</div>
-                <div class="flex mt-2 flex justify-between items-center">
-                    <div class="text-1xl font-light">${item.data}</div>
-                    <button data-button class="py-2">
-                        <img src="./assets/icons/points.svg" alt="points" />
-                    </button>
-                </div>
-                <div data-menu class="absolute bottom-[-7px] right-0 z-[2] opacity-0 pointer-events-none duration-500">
-                    <div class="bg-[#2F2C33] rounded-[25px] mb-2 flex p-3.5 gap-x-4 items-center">
-                        <button data-edit>
-                            <img class="w-6" src="./assets/icons/pencil.svg" alt="pencil" />
-                        </button>
-                        <button data-save>
-                            <img class="w-6" src="./assets/icons/${true? "pin": ""}.svg" alt="save" />
-                        </button>
-                        <button data-delete data-id="${i}">
-                            <img class="w-6" src="./assets/icons/trash.svg" alt="trash" />
-                        </button>
-                    </div>
-                </div>
-            </div>
-        `;
-    }
-
     addNotesToPage() {
         if (this.notes.length > 0) {
-            this.wrapper.classList.remove("flex");
+            this.wrapper.classList.remove("flex", "min-h-full");
             this.wrapper.classList.add("grid", "gap-6");
             this.wrapper.innerHTML = "";
             const pin = this.notes.filter(item => item.isPin);
             const unpin = this.notes.filter(item => !item.isPin);
-            const newList = pin.concat(unpin)
+            const newList = pin.concat(unpin);
 
             newList.forEach((item, i) => {
                 this.wrapper.innerHTML += `
-                    <div class="${item.isPin? "bg-[#3A3644]/25": "bg-[#3A3644]"} p-6 rounded-xl w-full relative">
-                        <div class="text-3xl font-light pb-6 relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full
-                            before:h-[1px] before:bg-[#77747D] overflow-hidden text-ellipsis whitespace-nowrap">${item.value}</div>
+                    <div class="${item.isPin? "bg-[#3A3644]/25": "bg-[#3A3644]"} p-4 md:p-6 rounded-xl w-full relative">
+                        <div class="text-xl md:text-3xl font-light pb-2 md:pb-6 relative before:content-[''] before:absolute before:bottom-0 before:left-0
+                            before:w-full before:h-[1px] before:bg-[#77747D] overflow-hidden text-ellipsis whitespace-nowrap">${item.value}</div>
                         <div class="flex mt-2 flex justify-between items-center">
-                            <div class="text-1xl font-light">${item.data}</div>
+                            <div class="md:text-xl font-light">${item.data}</div>
                             <button data-button class="py-2">
                                 <img src="./assets/icons/points.svg" alt="points" />
                             </button>
@@ -112,14 +84,14 @@ class App {
                         <div data-menu class="absolute bottom-[-7px] right-0 z-[2] opacity-0 pointer-events-none duration-500">
                             <div class="bg-[#2F2C33] rounded-[25px] mb-2 flex p-3.5 gap-x-4 items-center">
                                 <button data-edit>
-                                    <img class="w-6" src="./assets/icons/pencil.svg" alt="pencil" />
+                                    <img class="w-4 md:w-6" src="./assets/icons/pencil.svg" alt="pencil" />
                                 </button>
                                 <button data-pin data-key="${item.key}" class="button relative ${item.isPin && "active"}">
-                                    <img class="w-6" src="./assets/icons/unpin.svg" alt="save" />
-                                    <img class="w-6 absolute top-0 left-0 duration-500 opacity-0" src="./assets/icons/pin.svg" alt="save" />
+                                    <img class="w-4 md:w-6" src="./assets/icons/unpin.svg" alt="save" />
+                                    <img class="w-4 md:w-6 absolute top-0 left-0 duration-500 opacity-0" src="./assets/icons/pin.svg" alt="save" />
                                 </button>
                                 <button data-delete data-id="${i}">
-                                    <img class="w-6" src="./assets/icons/trash.svg" alt="trash" />
+                                    <img class="w-4 md:w-6" src="./assets/icons/trash.svg" alt="trash" />
                                 </button>
                             </div>
                         </div>
@@ -128,7 +100,8 @@ class App {
             });
 
             document.querySelector("body").innerHTML += `
-                <a href="./edit.html" class="absolute bottom-4 left-2/4 -translate-x-2/4 block h-24 w-24 rounded-full bg-[#9990A1] flex items-center justify-center">
+                <a href="./edit.html" class="absolute bottom-4 left-2/4 -translate-x-2/4 block h-16 md:h-24 w-16 md:w-24 rounded-full bg-[#9990A1] flex
+                    items-center justify-center">
                     <img src="./assets/icons/plus.svg" alt="plus">
                 </a>
             `;
